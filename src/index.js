781e5fc3193f4eacb75a1f70bff8850875ba690d
ofisch <65644771+ofisch@users.noreply.github.com>
2023-01-09 15:33:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('Hello console!');
+
+const a = 6;
+let b = 3;
+{
+    const a = 9;
+    const c = 3;
+    {
+        let c = 1;
+        console.log(a + b+ c);
+        b = 2;
+    }
+    console.log(a + b);
+}
+console.log(a+ b);
