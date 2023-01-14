202691cd183e3c0dd5859b54cdc5dacb9d03b03c
ofisch <onni.fischer@gmail.com>
2023-01-14 13:13:56 +0200

    task4-tehty

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 'use strict';
 
 import Menu from './assets/menu.json';
-//console.log('menu.json', Menu);
-
-let lang = 'fi';
 
 const courseMenu = Menu.courses;
 
+const coursesFi = [];
+const coursesEn = [];
 
+let activeMenu = coursesFi;
+let lang = 'fi';
 
 /**
- * Renders menu content to html page
- * @param {Array} menu - array of dishes
+ * Hakee ruokalajien otsikot Json-tiedostosta ja tallentaa listoihin
+ * @param {*} menu 
  */
+const getMenuJson = (menu) => {
+for (let i in menu) {
+  coursesFi[i-1] = menu[i].title_fi;
+  coursesEn[i-1] = menu[i].title_en;
+}
+};
 
+getMenuJson(courseMenu);
+
+/**
+ * Tulostaa ruokalajit sivulle
+ * @param {Array} menu - lista ruokalajeista
+ */
 const renderMenu = (menu) => {
   const menuBox = document.querySelector('#textbox');
   menuBox.innerHTML = "";
   const list = document.createElement('ul');
-  let dishes = [];
-  for (let x in menu) {
-    if (lang == 'fi') {
-      dishes[x] = menu[x].title_fi;
-    } else {
-      dishes[x] = menu[x].title_en;
-    }
+  for (let dish of menu) {
     const li = document.createElement('li');
-    li.textContent = dishes[x];
-    list.appendChild(li);
+    li.textContent = dish;
+    list.append(li);
   }
   menuBox.append(list);
 };
 
 // tulostetaan ruokalajit sivulle
-renderMenu(courseMenu);
+renderMenu(activeMenu);
 
 // haetaan radionapit sivulta
 let radioButtons = document.getElementsByName("sort");
@@ -53,57 +60,52 @@ const getRadioValue = () => {
 };
 
 /**
- * Sorts menu alphabetically
- * @param {Array} menu - Array of dishes
- * @param {string} order - 'asc' or 'desc'
- * @returns sorted menu array
+ * Järjestää ruokalajit aakkosjärjestykseen
+ * @param {Array} menu - lista ruokalajeista
+ * @param {string} order - 'asc' tai 'desc'
+ * @returns järjestetyn ruokalaji-listan
  */
-
 const sortMenu = (menu, order = 'asc') => {
   let sortedArray;
-  let menuItems = [];
-  for (let x in menu) {
-    if (lang == 'fi') {
-      menuItems[x] = menu[x].title_fi;
-    } else {
-      menuItems[x] = menu[x].title_en;
-    }
-  }
-
   if (order == "asc") {
-    sortedArray = menuItems.sort((a, b) => a.localeCompare(b));
-    console.log(sortedArray);
-    return sortedArray;
+      sortedArray = menu.sort((a, b) => a.localeCompare(b));
+      return sortedArray;
   } else {
-    sortedArray = menuItems.sort((a, b) => -1 * a.localeCompare(b));
-    console.log(sortedArray);
-    return sortedArray;
+      sortedArray = menu.sort((a, b) => -1 * a.localeCompare(b));
+      return sortedArray;
   }
 };
 
 /**
- * Change UI language
- * @param {string} language - 'fi' or 'en'
+ * Vaihtaa käyttöliittymän kielen
+ * @param {string} language - 'fi' tai 'en'
  */
 const changeLang = (language) => {
-  if (language == 'fi') {
-    //activeMenu = coursesFi;
+  if (language == 'fi'){
+    activeMenu = coursesFi;
   } else if (language == 'en') {
-    //activeMenu = coursesEn;
+    activeMenu = coursesEn;
   }
   lang = language;
-  renderMenu(courseMenu);
-
+  renderMenu(activeMenu);
 };
 
+/**
+ * Hakee satunnaisen ruokalajin 
+ * @param {*} menu 
+ * @returns satunnaisen ruokalajin
+ */
 const getRandomDish = (menu) => {
   const randomIndex = Math.floor(Math.random() * menu.length);
   return menu[randomIndex];
 };
 
+
+// EventListenerit napeille
+
 const sortButton = document.querySelector('#sortbutton');
 sortButton.addEventListener('click', () => {
-    renderMenu(sortMenu(courseMenu, getRadioValue()));
+    renderMenu(sortMenu(activeMenu, getRadioValue()));
 });
 
 const langButton = document.querySelector('#langbutton');
