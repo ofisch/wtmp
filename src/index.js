a6ac1906cd4a769e1bb78c3ff19df2d69010515a
ofisch <onni.fischer@gmail.com>
2023-01-14 13:15:53 +0200

    task3-tehty

--- src/index.js
+++ src/index.js
@@ -1,97 +1,105 @@
-//pienin ja suurin mahdollinen luku
-const min = 1;
-const max = 100;
-let randomNumber = Math.floor(Math.random() * max - min + 1) + min;
-console.log(randomNumber);
+'use strict';
 
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-const time = document.querySelector('.time');
+const coursesEn = ["Hamburger, cream sauce and poiled potates",
+"Goan style fish curry and whole grain rice",
+"Vegan Chili sin carne and whole grain rice",
+"Broccoli puree soup, side salad with two napas",
+"Lunch baguette with BBQ-turkey filling",
+"Cheese / Chicken / Vege / Halloum burger and french fries"];
 
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
+const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+"Goalaista kalacurrya ja täysjyväriisiä",
+"vegaani Chili sin carne ja täysjyväriisi",
+"Parsakeittoa,lisäkesalaatti kahdella napaksella",
+"Lunch baguette with BBQ-turkey filling",
+"Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
 
-let guessCount = 1;
-// arvausten enimmäismäärä
-const maxGuessCount = 10;
-let resetButton;
+let lang = 'fi';
+let activeMenu = coursesFi;
 
-// oikeaan arvaukseen mennyt aika
-let firstGuess, lastGuess, guessTime;
+/**
+ * Renders menu content to html page
+ * @param {Array} activeMenu - array of dishes
+ */
 
-guessField.focus();
+const renderMenu = (activeMenu) => {
+  const menuBox = document.querySelector('#textbox');
+  menuBox.innerHTML = "";
+  const list = document.createElement('ul');
+  for (const dish of activeMenu) {
+    const li = document.createElement('li');
+    li.textContent = dish;
+    list.appendChild(li);
+  }
 
-// millisekuntit sekunneiksi
-const millisToSec = (millis) => {
-    let sec = ((millis % 60000) / 1000).toFixed(0);
-    return sec + ' sekuntia';
+  menuBox.append(list);
 };
+renderMenu(coursesFi);
 
-const checkGuess = () => {
-    const userGuess = Number(guessField.value);
-    if (guessCount === 1) {
-        guesses.textContent = 'Previous guesses: ';
-        firstGuess = Date.now();
-      }
-      guesses.textContent += `${userGuess} `;
-    
-      if (userGuess === randomNumber) {
-        lastResult.textContent = 'Congratulations! You got it right!';
-        lastResult.style.backgroundColor = 'green';
-        // arvausten määrä
-        lowOrHi.textContent = 'Number of guesses: ' + guessCount;
-        // arvaukseen menneen ajan näyttäminen
-        lastGuess = Date.now();
-        guessTime = millisToSec(lastGuess - firstGuess);
-        time.textContent = 'Time spent guessing: ' + guessTime;
-        setGameOver();
-      } else if (guessCount === maxGuessCount) {
-        lastResult.textContent = '!!!GAME OVER!!!';
-        lowOrHi.textContent = '';
-        setGameOver();
-      } else {
-        lastResult.textContent = 'Wrong!';
-        lastResult.style.backgroundColor = 'red';
-        if (userGuess < randomNumber) {
-          lowOrHi.textContent = 'Last guess was too low!';
-        } else if (userGuess > randomNumber) {
-          lowOrHi.textContent = 'Last guess was too high!';
-        }
-      }
-    
-      guessCount++;
-      guessField.value = '';
-      guessField.focus();
-};
+let radioButtons = document.getElementsByName("sort");
 
-guessSubmit.addEventListener('click', checkGuess);
-
-const setGameOver = () => {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Start new game';
-    document.body.append(resetButton);
-    resetButton.addEventListener('click', resetGame);
-};
-
-const resetGame = () => {
-    guessCount = 1;
-
-    const resetParas = document.querySelectorAll('.resultParas p');
-    for (const resetPara of resetParas) {
-      resetPara.textContent = '';
+const getRadioValue = () => {
+let selectedRadio;
+for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+        selectedRadio = radioButtons[i].value;
     }
-  
-    resetButton.parentNode.removeChild(resetButton);
-  
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = '';
-    guessField.focus();
-  
-    lastResult.style.backgroundColor = 'white';
-  
-    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+    return selectedRadio;
 };
+
+/**
+ * Sorts menu alphabetically
+ * @param {Array} menu - Array of dishes
+ * @param {string} order - 'asc' or 'desc'
+ * @returns sorted menu array
+ */
+
+const sortMenu = (menu, order='asc') => {
+  let sortedArray;
+  if (order == "asc") {
+      sortedArray = menu.sort((a, b) => a.localeCompare(b));
+      return sortedArray;
+  } else {
+      sortedArray = menu.sort((a, b) => -1 * a.localeCompare(b));
+      return sortedArray;
+  }
+};
+
+/**
+ * Change UI language
+ * @param {string} language - 'fi' or 'en'
+ */
+const changeLang = (language) => {
+  if (language == 'fi'){
+    activeMenu = coursesFi;
+  } else if (language == 'en') {
+    activeMenu = coursesEn;
+  }
+  lang = language;
+  renderMenu(activeMenu);
+
+};
+
+const getRandomDish = (menu) => {
+  const randomIndex = Math.floor(Math.random()*menu.length);
+  return menu[randomIndex];
+};
+
+const sortButton = document.querySelector('#sortbutton');
+sortButton.addEventListener('click', () => {
+  renderMenu(sortMenu(coursesFi, getRadioValue()));
+});
+
+const langButton = document.querySelector('#langbutton');
+langButton.addEventListener('click', () => {
+  if (lang == 'fi')
+    changeLang('en');
+  else
+    changeLang('fi');
+});
+
+const randomButton = document.querySelector('#randombutton');
+randomButton.addEventListener('click', () => {
+  alert(getRandomDish(activeMenu));
+});
