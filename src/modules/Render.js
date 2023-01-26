5396a102ea5c60ae4f403e1d86b074adfc8749ee
ofisch <onni.fischer@gmail.com>
2023-01-26 10:33:46 +0200

    task2-tehty

--- src/modules/Render.js
+++ src/modules/Render.js
@@ -5,8 +5,10 @@
  * @param {Array} menu - lista ruokalajeista
  */
 const renderMenu = (menu) => {
-    const menuBox = document.querySelector('#textbox');
-    menuBox.innerHTML = "";
+    const menuBox = document.querySelector('.node-title').getElementsByTagName('p')[0];
+    console.log('boxi', menuBox);
+    //const menuBox = nodeContent.getElementsByClassName('node-title').getElementsByTagName('p')[0];
+    menuBox.innerHTML = '';
     const list = document.createElement('ul');
     for (let dish of menu) {
         const li = document.createElement('li');
